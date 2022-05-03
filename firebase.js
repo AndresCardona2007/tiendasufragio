ab95bc6aede81db873fcd2fff6584bec5367659c
a@gmail.com <a@gmail.com>
2022-05-03 15:50:13 -0500

    a

---
+++ firebase.js
@@ -0,0 +1,22 @@
+  /*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBQjsgApyNAtmSfAoMY_TFGKa5qhEZJRfk",
+    authDomain: "la-tiendita-71085.firebaseapp.com",
+    projectId: "la-tiendita-71085",
+    storageBucket: "la-tiendita-71085.appspot.com",
+    messagingSenderId: "251239078214",
+    appId: "1:251239078214:web:2233901dcdde1adfb6ad47",
+    measurementId: "G-D40RP0DGM7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);*/
