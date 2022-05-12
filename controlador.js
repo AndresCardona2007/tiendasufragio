72a15cdf95af3230f87281509a9ad0519f28cdb9
a@gmail.com <a@gmail.com>
2022-05-12 17:08:30 -0500

    a

--- controlador.js
+++ controlador.js
@@ -3,133 +3,190 @@ let producto1={
     nombre:"Portatil HP I3",
     precio:2500000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%201.jfif?alt=media&token=dd866b32-b52f-432c-9864-1c66e5f88c50"
+    
 }
 
 let producto2={
     nombre:"Portatil HP I5",
     precio:2600000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%202%20HP.jfif?alt=media&token=5ca2f52f-413d-4a29-837a-15c3d191557f"
+    
 }
 
 let producto3={
     nombre:"Portatil HP I7",
     precio:2800000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"corea",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%203%20HP.jfif?alt=media&token=4280775a-e45b-4b04-a29f-33ed9819d619"
+    
 }
 
 let producto4={
     nombre:"Tablet Lenovo Smart ",
     precio:500000,
     estado:true,
+    popularidad:"⭐⭐⭐⭐",
+    pais:"USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Tablet%204%20LENOVO.jfif?alt=media&token=e31d0d64-63c5-481b-9370-06ed0e1e7d68"
+    
 }
 
 let producto5={
     nombre:"Portatil ASUS I3",
     precio:1800000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%205%20ASUS.jfif?alt=media&token=92ebdce1-d46e-4e74-b436-6dfff7359e41"
+    
 }
 
 let producto6={
     nombre:"Portatil Lenovo I5",
     precio:2300000,
     estado:true,
+    popularidad:"⭐⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%206%20LENOVO.jfif?alt=media&token=0dd56e5c-42dc-42d5-add7-5b8b1de5b729"
+    
 }
 
 let producto7={
     nombre:"Portatil ASUS I5",
     precio:2200000,
     estado:true,
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%207%20ASUS.jfif?alt=media&token=8dead0c1-5c92-4c7c-af07-d6d16d1b24f2"
+    
 }
 
 let producto8={
     nombre:"Tablet Lenovo Smart TB",
     precio:500000,
     estado:true,
+    popularidad:"⭐⭐⭐⭐",
+    pais:"corea",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Tablet%208%20LENOVO.jfif?alt=media&token=7bdb2425-150d-4c21-a984-1921aa6788ae"
+    
 }
 
 let producto9={
     nombre:"Portatil HP Moderno",
     precio:3200000,
     estado:true,
+    popularidad:"⭐⭐⭐⭐",
+    pais:"corea",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%209%20HP.jfif?alt=media&token=811f47ff-4077-4ec6-86e1-819b04827666"
+    
 }
 
 let producto10={
     nombre:"Portatil Lenovo Moderno",
     precio:3300000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2010%20LENOVO.jfif?alt=media&token=5c1012ac-38b6-4b25-b2d3-8807ad6104f3"
+    
 }
 
 let producto11={
     nombre:"Portatil Lenovo Refrigeracion ",
     precio:3500000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2011%20LENOVO.jfif?alt=media&token=75bc9c07-02c3-40ae-8f37-fa8f6e0157d3"
+    
 }
 
 let producto12={
     nombre:"Portatil Lenovo Pantalla Moderna",
     precio:3100000,
     estado:true,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2012%20LENOVO.jfif?alt=media&token=48f604bb-0f88-4824-bc15-34dce83ad190"
+    
 }
 
 let producto13={
     nombre:"Portatil Lenovo Pequeño",
     precio:2500000,
     estado:true,
+    popularidad:"⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2013%20LENOVO.jfif?alt=media&token=4286a26c-ec7b-4e0c-b04b-a4dfe0d713c5"
+    
 }
 
 let producto14={
     nombre:"Portatil Table Lenovo",
     precio:4500000,
     estado:true,
+    popularidad:"⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2014%20LENOVO.jfif?alt=media&token=fcf94494-1e36-4a4b-a653-3b1e46edd0b1"
+    
 }
 
 let producto15={
     nombre:"Portatil Lenovo Gamer",
     precio:5500000,
     estado:false,
+    popularidad:"⭐⭐⭐",
+    pais:"corea",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2015%20LENOVO.jfif?alt=media&token=d18b95d1-764f-4ee2-b4be-43e0c785138b"
+    
 }
 
 let producto16={
     nombre:"Portatil HP Gamer",
     precio:4500000,
     estado:false,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2016%20HP.jfif?alt=media&token=07d9536d-401c-4d82-9f67-f2c42745c0be"
+    
 }
 
 let producto17={
     nombre:"Tablet Lenovo Gamer",
     precio:2500000,
     estado:false,
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:"USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Tablet%2017%20LENOVO.jfif?alt=media&token=abe31882-f392-4f83-b000-587a6cd91e4d"
+    
 }
 
 let producto18={
     nombre:"Portatil ACER Gamer",
     precio:3500000,
     estado:false,
+    popularidad:"⭐⭐⭐⭐⭐",
+    pais:"USA",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2018%20ACER.jfif?alt=media&token=aba7d3bf-2ff7-42d6-bd7a-9240dcf9c579"
+    
 }
 
 let producto19={
     nombre:"Portatil Lenovo I7 Gamer",
     precio:3800000,
     estado:false,
+    popularidad:"⭐⭐⭐",
+    pais:"china",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2019%20LENOVO.jfif?alt=media&token=75f26449-5196-4a6e-a766-c852e73072af"
+    
 }
 
 
@@ -137,7 +194,10 @@ let producto20={
     nombre:"Portatil HP I7 Gamer",
     precio:3200000,
     estado:false,
+    popularidad:"⭐⭐⭐",
+    pais:"corea",
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-71085.appspot.com/o/Portatil%2020%20HP.jfif?alt=media&token=b8bdf1a4-248d-42bd-98f8-8b48dbfeff15"
+    
 }
 
 
@@ -164,7 +224,7 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20 
+    producto20
 )
 
 let contenedor=document.getElementById("contenedor")
@@ -177,10 +237,43 @@ productos.forEach(function(producto){
     let columna=document.createElement("div")
     columna.classList.add("col")
     let tarjeta=document.createElement("div")
+    let nombreProducto=document.createElement("h5")
+    let precioProducto=document.createElement("h5")
+    let popularidadProducto=document.createElement("h5")
+    let paisProducto=document.createElement("h5")
+    nombreProducto.classList.add("text-center")
+    precioProducto.classList.add("text-center")
+    precioProducto.classList.add("bg-success")
+    popularidadProducto.classList.add("text-center")
+    popularidadProducto.classList.add("bg-light")
+    paisProducto.classList.add("text-center")
+    nombreProducto.textContent=producto.nombre
+    precioProducto.textContent= "$"+ producto.precio
+    popularidadProducto.textContent=producto.popularidad
+    paisProducto.textContent=producto.pais
+
+
+    let estadoProducto=document.createElement("h5")
+
+    if(producto.estado==true){
+    estadoProducto.classList.add("text-center")
+    estadoProducto.textContent="Estado Nuevo"    
+    }
+    else {
+     estadoProducto.classList.add("text-center")
+     estadoProducto.textContent="Estado usado"   
+    }
+
     tarjeta.classList.add("card")
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(estadoProducto)
+    tarjeta.appendChild(popularidadProducto)
+    tarjeta.appendChild(paisProducto)
     columna.appendChild (tarjeta)
     contenedor.appendChild(columna)
+    
 
 
 
